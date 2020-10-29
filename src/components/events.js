66d0f269ca5b16382ffad2f11538b7be44205267
mahadevinc <mahadevinc2000@gmail.com>
2020-10-29 15:02:41 +0530

    experimented one

--- src/components/events.js
+++ src/components/events.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
+import { Jumbotron } from 'react-bootstrap';
+import { Events } from "./images/events.png";
 
 class Event extends Component {
     render() {
         return(
-            <div className="events">
-                <h3>Events</h3>
+        
+            <div>
+
+                <Jumbotron className="eventsHeader">
+                <section className="section-notes" id="services">
+                    <p className="sectiontag text-medium text1" >
+                        EVENTS
+                    </p>
+                    <div className="thumb-notes">
+                        <img src={ Events } alt=" Events " className="imgfluid" />
+                    </div>
+                    
+                </section>
+                </Jumbotron>
+
+                
             </div>
+            
         )
     }
 }
